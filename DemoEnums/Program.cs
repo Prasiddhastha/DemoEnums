8074f0f4e54df913cef3911df575f2ae37397922
Prasiddha Shrestha <prasiddhastha7@gmail.com>
2022-12-22 20:52:00 +0545

    Add project files.

---
+++ DemoEnums/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace DemoEnums
+{   
+    //enum
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express=3
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.RegularAirMail;
+            Console.WriteLine((int)method);//numeric value of that enum by casting to integer
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);//converting integer to shipping method
+
+            Console.WriteLine(method.ToString());//convert enum to string
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod) Enum.Parse(typeof(ShippingMethod), methodName);//string to enum
+            Console.WriteLine(shippingMethod);
+            
+        }
+    }
+}
